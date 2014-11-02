7c56e8af19d22fa70ea1121fac9a036fee1fbfc4
TusharBabbar <babbartushar@gmail.com>
2014-11-02 19:43:04 +0530

    changes in oauth

--- app/resources.py
+++ app/resources.py
@@ -6,9 +6,13 @@ from request_parsers import *
 from datetime import datetime
 from models import *
 
+session = dict()
+
 class TwitterAuth(restful.Resource):
     def get(self):
         oauth = tweepy.OAuthHandler(current_app.config['CONSUMER_KEY'], current_app.config['CONSUMER_SECRET'], secure=True)
+        session['request_token'] = (auth.request_token.key,
+                    auth.request_token.secret)
         oauth.callback = url_for('twitter_callback', _external=True)
         auth_url = oauth.get_authorization_url()
         return redirect(auth_url)
@@ -21,7 +25,10 @@ class TwitterCallback(restful.Resource):
     def get(self):
         try:
             oauth = tweepy.OAuthHandler(current_app.config['CONSUMER_KEY'], current_app.config['CONSUMER_SECRET'], secure=True)
+            token = session['request_token']
+            del session['request_token']
             verifier = request.args.get('oauth_verifier')
+
             oauth.get_access_token(verifier)
             api = tweepy.Api(oauth.access_token())
             u = api.verify_credentails()
