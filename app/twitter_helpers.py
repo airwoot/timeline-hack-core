4ce5c6b55de71e37f4204416e6ad1552bccfb366
TusharBabbar <babbartushar@gmail.com>
2014-11-03 15:38:50 +0530

    added methods for managing list in twitter

--- app/twitter_helpers.py
+++ app/twitter_helpers.py
@@ -11,13 +11,27 @@ class TwitterUser():
                     access_token_secret = access_token_secret
                 )
 
-    def get_list_timeline(timeline_id, user_id, since_id, count = 25):
+    def get_list_timeline(timeline_id, user_id, since_id, count = 10):
+        count = 100 if count > 100 else count
         res = api.GetListTimeline(timeline_id , slug = None, owner_id = owner_id, count = count)
         return [r.AsDict() for r in res]
         
     def get_user_lists(user_twitter_id):
         res = api.GetLists()
         return [r.AsDict() for r in res]
+
+    def create_list(screen_name):
+        user = self.api.VerifyCredentials()
+        timeline_for = self.api.GetUser(screen_name = screen_name)
+        list_members = self.api.GetFriendIDs(timeline_for.id)
+        list_members = map(str, listmembers)
+        timeline_list = self.api.CreateList(timeline_for.screen_name + '_sees')
+        self.api.CreateListsMember(timeline_list.id, user.id, list_members[:35])
+        #create cel task to add all the members to the list
+        return timeline_list
+
+    def subscribe_list(list_id, owner_id):
+        return self.api.CreateSubscription(owner_id = owner_id, list_id = list_id)
 
     def post_tweet(self, tweet):
         print 'posting:',tweet
